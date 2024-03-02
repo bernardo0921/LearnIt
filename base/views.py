f14d9ab3bb5b8dcb5d0e99eeb9327658a1ecd16c
bernardo0921 <ephraimbernard7@gmail.com>
2024-03-02 07:16:07 +0000

    added room list in view

--- base/views.py
+++ base/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 
+rooms = [
+    {'id': 1, 'name': 'what is matter in the real world'},
+    {'id': 1, 'name': 'Who is Jesus'},
+    {'id': 1, 'name': 'I am in Ghana'},
+]
 # Create your views here.
 def Home(request):
+    contents = {"rooms": rooms}
     return render(request, "Home.html")
 
 def Students(request):
