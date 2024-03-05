ec7de5a14f453e350310a7e1b05be3ff197666f2
bernardo0921 <ephraimbernard7@gmail.com>
2024-03-05 01:04:44 +0000

    added a model

--- base/models.py
+++ base/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Room(models.Model):
+    # host = 
+    # topic = 
+    name = models.CharField(max_length=200)
+    description = models.TextField(null=True, blank=True)
+    # Participants = models
+    updated = models.DateTimeField(auto_now=True, auto_now_add=False)
+    created = models.DateTimeField(auto_now=False, auto_now_add=True)
+    
+    def __str__(self):
+        return self.name
+    
