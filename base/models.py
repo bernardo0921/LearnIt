0e8a20713d4dbfbbb28f19a27dade933a9ec6ce7
bernardo0921 <ephraimbernard7@gmail.com>
2024-03-13 17:20:15 +0000

    imported users to models.py

--- base/models.py
+++ base/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User 
 
 # Create your models here.
 class Room(models.Model):
@@ -15,7 +16,7 @@ class Room(models.Model):
     
 # creating one to many relationship
 class Message(models.Model):
-    # user = 
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     room = models.ForeignKey(Room, on_delete=models.CASCADE)
     body = models.TextField()
     updated = models.DateTimeField(auto_now=True, auto_now_add=False)
--- base/urls.py
+++ base/urls.py
@@ -3,8 +3,7 @@ from .views import Home, Students, indexes
 
 # create url patterns for the base app
 urlpatterns = [
-    path('', Home, name = 'home'),
+    path('', indexes, name = "indexes"),
     path('student/<str:pk>', Students, name = "student"),
-    path('indexes/', indexes, name = "indexes"),
-    # what is mutex
+    
 ]
